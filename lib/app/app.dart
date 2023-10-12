b9fa22bae3836d334733da32f30df9ea79f1fe31
Usama Javed <usama.javaid1919@gmail.com>
2023-10-12 17:48:48 +0500

    starting todo app

---
+++ lib/app/app.dart
@@ -0,0 +1,28 @@
+import 'package:todo/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:todo/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:todo/ui/views/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: StartupView),
+    // @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
